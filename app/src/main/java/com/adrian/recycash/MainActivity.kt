6642b5d3b9ae92a75539fb3c915ed2a219945f0c
adriantriputra <adriantriputra.97@gmail.com>
2023-05-22 10:47:21 +0700

    Customize bottom nav

--- app/src/main/java/com/adrian/recycash/MainActivity.kt
+++ app/src/main/java/com/adrian/recycash/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.adrian.recycash.databinding.ActivityMainBinding
 
@@ -24,9 +22,9 @@ private lateinit var binding: ActivityMainBinding
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_history, R.id.navigation_exchange, R.id.navigation_profile))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.navigation_home, R.id.navigation_history, R.id.navigation_exchange, R.id.navigation_profile))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
