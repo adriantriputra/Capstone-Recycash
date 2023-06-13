3b6e7b9b001458db173a713398b404f8fe031de1
adriantriputra <adriantriputra.97@gmail.com>
2023-06-13 22:18:52 +0700

    Finalizing exchange fragment

--- app/src/main/java/com/adrian/recycash/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/adrian/recycash/ui/exchange/ExchangeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.adrian.recycash.R
 import com.adrian.recycash.databinding.FragmentExchangeBinding
 
 class ExchangeFragment : Fragment() {
@@ -21,8 +23,57 @@ class ExchangeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentExchangeBinding.inflate(inflater, container, false)
-        return binding.root
+        val root: View = binding.root
+
+        val imgGopay = binding.imgGopay
+        val imgShopeepay = binding.imgShopeepay
+        val imgOvo = binding.imgOvo
+        val imgDana = binding.imgDana
+        val imgLinkAja = binding.imgLinkaja
+        val imgDoku = binding.imgDoku
+        val imgIsaku = binding.imgIsaku
+
+        val imgAlfagift = binding.imgAlfagift
+        val imgKopken = binding.imgKopiKenangan
+        val imgStarbucks = binding.imgStarbucks
+        val imgPulsa = binding.imgPulsa
+
+        val toastClickListener = View.OnClickListener {
+            val message = when (it.id) {
+                R.id.img_gopay -> getString(R.string.coming_soon)
+                R.id.img_shopeepay -> getString(R.string.coming_soon)
+                R.id.img_ovo -> getString(R.string.coming_soon)
+                R.id.img_dana -> getString(R.string.coming_soon)
+                R.id.img_linkaja -> getString(R.string.coming_soon)
+                R.id.img_doku -> getString(R.string.coming_soon)
+                R.id.img_isaku -> getString(R.string.coming_soon)
+                R.id.img_alfagift -> getString(R.string.coming_soon)
+                R.id.img_kopi_kenangan -> getString(R.string.coming_soon)
+                R.id.img_starbucks -> getString(R.string.coming_soon)
+                R.id.img_pulsa -> getString(R.string.coming_soon)
+                else -> null
+            }
+
+            message?.let { msg ->
+                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        imgGopay.setOnClickListener(toastClickListener)
+        imgShopeepay.setOnClickListener(toastClickListener)
+        imgOvo.setOnClickListener(toastClickListener)
+        imgDana.setOnClickListener(toastClickListener)
+        imgLinkAja.setOnClickListener(toastClickListener)
+        imgDoku.setOnClickListener(toastClickListener)
+        imgIsaku.setOnClickListener(toastClickListener)
+        imgAlfagift.setOnClickListener(toastClickListener)
+        imgKopken.setOnClickListener(toastClickListener)
+        imgStarbucks.setOnClickListener(toastClickListener)
+        imgPulsa.setOnClickListener(toastClickListener)
+
+        return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
